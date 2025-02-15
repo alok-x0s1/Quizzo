2d060b8f34d00adf070d45996e66e360cf699f4f
Alok Yadav <yalok6321@gmail.com>
2025-02-15 11:27:40 +0530

    frontend config, utils, and assets

---
+++ frontend/src/utils/validator.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+export const contactFormSchema = z.object({
+	name: z.string().min(2, {
+		message: "Name must be at least 2 characters.",
+	}),
+	email: z.string().email({
+		message: "Please enter a valid email address.",
+	}),
+	message: z.string().min(10, {
+		message: "Message must be at least 10 characters.",
+	}),
+});
+
+export const loginFormSchema = z.object({
+	username: z.string().min(2, {
+		message: "Username must be at least 2 characters.",
+	}),
+	password: z.string().min(6, {
+		message: "Password must be at least 6 characters.",
+	}),
+});
+
+export const signupFormSchema = z.object({
+	username: z.string().min(2, {
+		message: "Username must be at least 2 characters.",
+	}),
+	password: z.string().min(6, {
+		message: "Password must be at least 6 characters.",
+	}),
+});
+
+export const createQuizSchema = z.object({
+	title: z.string().min(2, {
+		message: "Title must be at least 2 characters.",
+	}),
+	description: z.string().min(10, {
+		message: "Description must be at least 10 characters.",
+	}),
+});
